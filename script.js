516fdb2ca2fbb05db7813b2108f63ee39a0d9f98
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-06-22 16:27:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// This is the boilerplate code given for you
-// You can modify this code
 // Product data
 const products = [
   { id: 1, name: "Product 1", price: 10 },
@@ -11,6 +9,11 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartButton = document.getElementById("clear-cart-btn");
+
+// Cart data
+let cart = [];
 
 // Render product list
 function renderProducts() {
@@ -22,17 +25,71 @@ function renderProducts() {
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+  cartList.innerHTML = "";
+  cart.forEach((cartItem) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${cartItem.name} - $${cartItem.price} <button class="remove-from-cart-btn" data-id="${cartItem.id}">Remove</button>`;
+    cartList.appendChild(li);
+  });
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+  const product = products.find((item) => item.id === productId);
+  if (product) {
+    cart.push(product);
+    renderCart();
+    updateCartInStorage();
+  }
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+  cart = cart.filter((item) => item.id !== productId);
+  renderCart();
+  updateCartInStorage();
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+  cart = [];
+  renderCart();
+  updateCartInStorage();
+}
+
+// Update cart data in session storage
+function updateCartInStorage() {
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// Initialize cart data from session storage
+function initializeCartFromStorage() {
+  const cartData = sessionStorage.getItem("cart");
+  if (cartData) {
+    cart = JSON.parse(cartData);
+    renderCart();
+  }
+}
+
+// Event listeners
+productList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("add-to-cart-btn")) {
+    const productId = parseInt(event.target.getAttribute("data-id"));
+    addToCart(productId);
+  }
+});
+
+cartList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const productId = parseInt(event.target.getAttribute("data-id"));
+    removeFromCart(productId);
+  }
+});
+
+clearCartButton.addEventListener("click", clearCart);
 
 // Initial render
 renderProducts();
-renderCart();
+initializeCartFromStorage();
+
